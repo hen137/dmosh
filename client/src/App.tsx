6ce8cd0df14cc7d5e41c637d5a19ff4e1f2a9012
Henry Abramovich <henry.abramovich@gmail.com>
2025-04-30 22:47:13 -0400

    basic video js support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <>
       <Stream />
-      <Screen />
+      {/* <Screen /> */}
     </>
   );
 };
--- client/src/components/stream/stream.tsx
+++ client/src/components/stream/stream.tsx
@@ -1,65 +1,66 @@
 // VideoPlayer.js
 import { useRef, useEffect } from 'react';
 import videojs, { VideoJsPlayer } from 'video.js';
+import '../../../node_modules/video.js/dist/video-js.css'
 import './stream.css'
 
+// const options = {
+//   autoplay: true,
+//   controls: false,
+//   fluid: false,
+//   mute: true,
+//   sources: [
+//     {
+//       src: "http://localhost:3001/video",
+//       type: "video/mp4"
+//     }
+//   ]
+// };
+
 const Stream = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLDivElement>(null);
   const playerRef = useRef<VideoJsPlayer>(null);
-
-  const options = {
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    mute: true,
-    sources: [{
-      src: "localhost:3001/video/001",
-      type: "video/mp4"
-    },
-    ]
-  };
-
-  const onReady = (player: VideoJsPlayer) => {
-    player.on('ended', () => {
-      console.log('Video Ended');
-    });
-  };
 
   useEffect(() => {
     // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      const videoElement = document.createElement("video-js");
+    if (!playerRef.current && videoRef.current) {
+      const videoElement = document.createElement("video");
+      videoElement.classList.add('vjs-big-play-centered');
       videoRef.current.appendChild(videoElement); //ignore error - typescript & video.js dont play nice
 
-      const player = playerRef.current = videojs(videoElement, options, () => {
-        console.log('player is ready');
-        onReady && onReady(player);
+      playerRef.current = videojs(videoElement, {
+        autoplay: true,
+        loop: true,
+        muted: true,
+        controls: false,
+        disablePictureInPicture: true,
+        // fluid: true,
+        sources: [
+          {
+            src: "http://localhost:3001/video",
+            type: "video/mp4"
+          }
+        ]
       });
-
-      // You can update the player in the `onReady` callback only
-      // on component update, for example:
-    } else {
-      const player = playerRef.current;
-
-      player.autoplay(options.autoplay);
-      player.src(options.sources);
     }
+  }, [videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
 
     return () => {
-      // Destroy player on unmount
-      if (playerRef.current) {
-        playerRef.current.dispose();
+      if (player && !player.isDisposed()) {
+        player.dispose();
         playerRef.current = null;
       }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [playerRef])
 
   return (
     <div className='stream'>
-      <video ref={videoRef} className="" />
+      <div ref={videoRef} className="" />
     </div>
+    // <div ref={videoRef} className='stream' />
   );
 };
 
