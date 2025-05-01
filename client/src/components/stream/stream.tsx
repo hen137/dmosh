9d0141638a5f50fdcb5107652ab6bccf06505a44
Henry Abramovich <henry.abramovich@gmail.com>
2025-05-01 16:06:30 -0400

    moved options definition

--- client/src/components/stream/stream.tsx
+++ client/src/components/stream/stream.tsx
@@ -4,18 +4,20 @@ import videojs, { VideoJsPlayer } from 'video.js';
 import '../../../node_modules/video.js/dist/video-js.css'
 import './stream.css'
 
-// const options = {
-//   autoplay: true,
-//   controls: false,
-//   fluid: false,
-//   mute: true,
-//   sources: [
-//     {
-//       src: "http://localhost:3001/video",
-//       type: "video/mp4"
-//     }
-//   ]
-// };
+const options = {
+  autoplay: true,
+  loop: true,
+  muted: true,
+  controls: false,
+  disablePictureInPicture: true,
+  fluid: true,
+  sources: [
+    {
+      src: "http://localhost:3001/video",
+      type: "video/mp4"
+    }
+  ]
+}
 
 const Stream = () => {
   const videoRef = useRef<HTMLDivElement>(null);
@@ -24,24 +26,10 @@ const Stream = () => {
   useEffect(() => {
     // Make sure Video.js player is only initialized once
     if (!playerRef.current && videoRef.current) {
-      const videoElement = document.createElement("video");
-      videoElement.classList.add('vjs-big-play-centered');
-      videoRef.current.appendChild(videoElement); //ignore error - typescript & video.js dont play nice
+      const videoElement = document.createElement("video-js");
+      videoRef.current.appendChild(videoElement);
 
-      playerRef.current = videojs(videoElement, {
-        autoplay: true,
-        loop: true,
-        muted: true,
-        controls: false,
-        disablePictureInPicture: true,
-        // fluid: true,
-        sources: [
-          {
-            src: "http://localhost:3001/video",
-            type: "video/mp4"
-          }
-        ]
-      });
+      playerRef.current = videojs(videoElement, options);
     }
   }, [videoRef]);
 
@@ -57,10 +45,7 @@ const Stream = () => {
   }, [playerRef])
 
   return (
-    <div className='stream'>
-      <div ref={videoRef} className="" />
-    </div>
-    // <div ref={videoRef} className='stream' />
+    <div ref={videoRef} className="stream" />
   );
 };
 
